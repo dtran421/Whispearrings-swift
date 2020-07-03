74e6a9b031a3b2e4dc3825e0f51378751737bd8e
Duke <duketran2001@gmail.com>
2020-07-03 16:06:23 -0400

    Initial Commit

---
+++ Whispearrings/ContentView.swift
@@ -0,0 +1,42 @@
+//
+//  ContentView.swift
+//  Whispearrings
+//
+//  Created by Duke Tran on 7/3/20.
+//  Copyright © 2020 Whispearrings. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var selection = 0
+ 
+    var body: some View {
+        TabView(selection: $selection){
+            Text("First View")
+                .font(.title)
+                .tabItem {
+                    VStack {
+                        Image("first")
+                        Text("First")
+                    }
+                }
+                .tag(0)
+            Text("Second View")
+                .font(.title)
+                .tabItem {
+                    VStack {
+                        Image("second")
+                        Text("Second")
+                    }
+                }
+                .tag(1)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
